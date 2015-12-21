00ff245bb62821d19b2420543003a58a2cf1a4ca
Simon Sticko <simon.sticko@it.uu.se>
2015-12-21 10:33:29 +0100

    plotLogarithmic.m now draws line with expected slope through last point instead.

--- performanceTests/membraneConvergence.m
+++ performanceTests/membraneConvergence.m
@@ -4,13 +4,13 @@ function[]=membraneConvergence()
 close all;
 %make folder to put results in.
 cclock=clock();
-folderName=[num2str(cclock(2)) '_' num2str(cclock(3)) '_' ...
+folderName=[num2str(cclock(1)) '_' num2str(cclock(2)) '_' num2str(cclock(3)) '_' ...
     num2str(cclock(4)) '_' num2str(cclock(5))];
 path=['results/waveDirichlet/' folderName '/'];
 mkdir(path);
 %n is "fineness" in space want to start with the most computational heavy
 %simulation therefore flipud
-n=flipud(10*(9:2:17)');
+n=flipud(2.^(3:7));
 nPeriods=1.125;
 h=zeros(size(n));
 uError=zeros(size(n));
@@ -27,13 +27,13 @@ for j=1:length(n)
     [uError(j),~,h(j),condM(j),boundaryError(j),XMaxError(j,:),...
         gradError(j),dudtError(j)]=fictitiousWave(nPeriods,n(j),max(n),false);
 end
-pString='$p$';
 color='b';
 %Plot errror
 figure(1);
 hold on;
 yLab='$\left\Vert  u_{h}-u \right\Vert $';
-plotLogarithmic(h,uError,'h',yLab,pString,color);
+expectedSlope=2;
+plotLogarithmic(h,uError,'h',yLab,expectedSlope,color);
 fixAndSave(path,'L2Error');
 %Plot condition number
 figure(2);
@@ -46,7 +46,8 @@ fixAndSave(path,'condM');
 figure(3);
 hold on;
 yLab='$\left\Vert u_{h}-g_{D}\right\Vert _{\Gamma_{D}}$';
-plotLogarithmic(h,boundaryError,'h',yLab,pString,color);
+expectedSlope=2;
+plotLogarithmic(h,boundaryError,'h',yLab,expectedSlope,color);
 fixAndSave(path,'boundaryError');
 %Plot where maximum error occurs.
 figure(4);
@@ -56,17 +57,19 @@ plot(h,rMaxError,[color 'o'],'linewidth',2);
 xlabel('h');
 ylabel('r of max error');
 fixAndSave(path,'maxError');
-%Plot boundaryError
+%Plot gradient error
 figure(5);
 hold on;
 yLab='$\left\Vert \nabla( u_{h}-u )\right\Vert$';
-plotLogarithmic(h,gradError,'h',yLab,pString,color);
+expectedSlope=1;
+plotLogarithmic(h,gradError,'h',yLab,expectedSlope,color);
 fixAndSave(path,'gradError');
 %Plot dudtError
 figure(6);
 hold on;
 yLab='$\left\Vert \dot{u}_{h}-\dot{u}\right\Vert$';
-plotLogarithmic(h,dudtError,'h',yLab,pString,color);
+expectedSlope=2;
+plotLogarithmic(h,dudtError,'h',yLab,expectedSlope,color);
 fixAndSave(path,'dudtError');
 save([path 'savedData' '.mat']);
 close all;
--- performanceTests/outerProblConvergence.m
+++ performanceTests/outerProblConvergence.m
@@ -4,13 +4,13 @@ function[]=outerProblConvergence()
 close all;
 %make folder to put results in.
 cclock=clock();
-folderName=[num2str(cclock(2)) '_' num2str(cclock(3)) '_' ...
+folderName=[num2str(cclock(1)) '_' num2str(cclock(2)) '_' num2str(cclock(3)) '_' ...
     num2str(cclock(4)) '_' num2str(cclock(5))];
 path=['results/outerProbl/' folderName '/'];
 mkdir(path);
 %n is "fineness" in space want to start with the most computational heavy
 %simulation therefore flipud
-n=flipud(10*(7:1:12)');
+n=flipud(2.^(3:7));
 Tend=1;
 %Return error, want to save them.
 h=zeros(size(n));
@@ -27,25 +27,28 @@ end
 save([path 'savedData' '.mat']);
 %error in u
 yLab='$\left\Vert  u_{h}-u \right\Vert $';
-plotAndSave(h,uError,yLab,path);
+expectedSlope=2;
+plotAndSave(h,uError,yLab,path,expectedSlope);
 %error in grad
 yLab='$\left\Vert \nabla( u_{h}-u )\right\Vert$';
-plotAndSave(h,gradError,yLab,path);
+expectedSlope=1;
+plotAndSave(h,gradError,yLab,path,expectedSlope);
 %dudt
 yLab='$\left\Vert \dot{u}_{h}-\dot{u}\right\Vert$';
-plotAndSave(h,dudtError,yLab,path);
+expectedSlope=2;
+plotAndSave(h,dudtError,yLab,path,expectedSlope);
 %boundary error
 yLab='$ \left \Vert \hat{n} \cdot \nabla u_{h} - g_{N} \right \Vert_{\Gamma_{N}}$';
-plotAndSave(h,bcError,yLab,path);
+expectedSlope=1;
+plotAndSave(h,bcError,yLab,path,expectedSlope);
 end
 
 %Resizes and saves figure.
-function[]=plotAndSave(h,error,yLab,path)
+function[]=plotAndSave(h,error,yLab,path,expectedSlope)
 fig=figure();
 name=inputname(2);
-pString='$p$';
 color='b';
-plotLogarithmic(h,error,'h',yLab,pString,color);
+plotLogarithmic(h,error,'h',yLab,expectedSlope,color);
 resizeFig();
 fixPaperSize();
 saveas(figure(fig),[path name '.fig'],'fig');
