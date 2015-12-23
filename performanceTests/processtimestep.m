51a75f2f68589548baba9ec27663523da72ab5ed
Simon Sticko <simon.sticko@it.uu.se>
2015-12-23 14:08:12 +0100

    Done with the experiment of time-step vs size of cut.

--- performanceTests/processtimestep.m
+++ performanceTests/processtimestep.m
@@ -1,14 +1,34 @@
-function[]=processtimestep()
-clear;
+function[]=processtimestep(ddate)
 close all;
-load timestep
-ploteigs(h,epsilon,stabilized);
+datafile=['results/timestep/' ddate '/savedData'];
+load(datafile)
+% plotEigs(h,epsilon,eigNoStab,[resultdir, 'noStab']);
+% plotEigs(h,epsilon,eigOnlyM,[resultdir, 'onlyM']);
+plotEigs(h,epsilon,eigOnlyA,[resultdir, 'onlyA']);
+plotEigs(h,epsilon,eigStab,[resultdir, 'stab']);
 end
 
-function[]=ploteigs(h,epsilon,eigenvalues)
-linewidth=2;
-hMat=h*ones(size(epsilon));
+function[]=plotEigs(h,epsilon,eigenvalues,savename)
 fig=figure();
-plot(h,log(hMat.^2.*eigenvalues),'linewidth',linewidth);
-
+linewidth=2;
+markersize=8;
+hMat=h*ones(size(epsilon));
+linespecs={':d','-.s','--^'};
+eigScaled=hMat.^2.*eigenvalues;
+hold on;
+for i=1:size(eigenvalues,1)
+    plot(log10(epsilon),log10(eigScaled(i,:)),linespecs{i},'linewidth',linewidth,...
+        'markersize',markersize);
+end
+hold off;
+%Fix legend
+formatfunction=@(s) ['h=' num2str(s,'%1.1e')];
+llegend=arrayfun(formatfunction, h, 'unif', 0);
+legend(llegend,'orientation','horizontal','location','NorthOutside');
+xlabel('$\log_{10}(\epsilon)$','interpreter','latex');
+ylabel(['$\log_{10}(h^2\lambda_{\max})$'],'interpreter','latex');
+resizeFig(14);
+fixPaperSize();
+saveas(fig,[savename ,'.pdf'],'pdf');
+saveas(fig,[savename ,'.fig'],'fig');
 end
--- performanceTests/sliverTimeStep.m
+++ performanceTests/sliverTimeStep.m
@@ -1,20 +1,30 @@
 function[]=sliverTimeStep()
 addpath('assembling')
-resultdir='results/timestep/';
-n=[7,40];
-epsilon=linspace(.7,.7,5);
+cclock=clock();
+folderName=[num2str(cclock(1)) '_' num2str(cclock(2)) '_' num2str(cclock(3)) '_' ...
+    num2str(cclock(4)) '_' num2str(cclock(5))];
+resultdir=['results/timestep/' folderName '/'];
+mkdir(resultdir);
+n=flipud(2.^(2:2:6)');
+epsilon=2.^(-15:1:-1);
 h=zeros(length(n),1);
-stabilized=zeros(length(n),length(epsilon));
-unstabilized=zeros(size(stabilized));
+eigNoStab=zeros(length(n),length(epsilon));
+eigOnlyM=zeros(length(n),length(epsilon));
+eigOnlyA=zeros(length(n),length(epsilon));
+eigStab=zeros(length(n),length(epsilon));
 for i=1:length(n)
     for j=1:length(epsilon)
-        [h(i),stabilized(i,j),unstabilized(i,j)]=solveOnce(n(i),epsilon(j));
+            disp(['i=' num2str(i) ' j=' num2str(j) ', time=']);
+            disp(num2str(clock()));
+        [h(i),eigNoStab(i,j),eigOnlyM(i,j),eigOnlyA(i,j),eigStab(i,j)]=solveOnce(n(i),epsilon(j));
     end
 end
-save([resultdir 'timestep']);
+save([resultdir 'savedData']);
 end
 
-function[h,eigStabilized,eigUnstabilized]=solveOnce(n,epsilon)
+
+
+function[h,eigNoStab,eigOnlyM,eigOnlyA,eigStab]=solveOnce(n,epsilon)
 xLim=[-1.5,1.5];
 yLim=xLim;
 h=diff(xLim)/(n-1);
@@ -29,9 +39,11 @@ cutMesh=CutMesh(xLim,yLim,n,XB,haveInnerProblem);
 gD=@(x,y) zeros(size(x));
 f=@(x,y) zeros(size(x));
 dirichletInner=true;
-dirichletOuter=false;
+dirichletOuter=true;
 %Need to choose
 [M,A,~,~,~,m,a]=assemble(cutMesh,f,gD,dirichletInner,gD,dirichletOuter);
-eigStabilized=eigs(A,M,1);
-eigUnstabilized=eigs(a,m,1);
+eigNoStab=eigs(a,m,1);
+eigOnlyM=eigs(a,M,1);
+eigOnlyA=eigs(A,m,1);
+eigStab=eigs(A,M,1);
 end
