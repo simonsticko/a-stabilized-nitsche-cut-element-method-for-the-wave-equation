9027b8e62e2a8c7145622856561b24260caa6ca3
Simon Sticko <simon.sticko@it.uu.se>
2015-12-23 19:09:46 +0100

    Switched back to incircle radius for mesh paramter.

--- assembling/CutMesh.m
+++ assembling/CutMesh.m
@@ -122,8 +122,12 @@ classdef CutMesh<handle
             y=linspace(ylim(1),ylim(2),ny);
             [Xm,Ym]=meshgrid(x,y);
             this.dt=delaunayTriangulation(Xm(:), Ym(:));
-            %Mesh parameter.
-            this.h=min(diff(x(1:2)),diff(y(1:2)));
+            %Mesh parameter, incircle diameter.
+            a=diff(x(1:2));
+            b=diff(y(1:2));
+            c=sqrt(a^2+b^2);
+            radius=a*b/(a+b+c);
+            this.h=2*radius;
         end
         
         %Loop through all boundary faces of the background triangulation.
